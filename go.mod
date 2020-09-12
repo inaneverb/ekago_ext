e39127c262210882f4bafd72a4211fecb2cb0be1
Ilya Yuryevich <qioalice@gmail.com>
2020-09-12 19:28:21 +0300

    internal/ekasql: Added SQL NULL constants

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/qioalice/ekago_ext/v2
+
+go 1.13
+
+require (
+	github.com/go-pg/pg/v10 v10.0.3
+	github.com/qioalice/ekago/v2 v2.7.1
+)
---
+++ internal/ekasql/null.go
@@ -0,0 +1,16 @@
+// Copyright © 2020. All rights reserved.
+// Author: Ilya Stroy.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
+package ekasql
+
+//goland:noinspection GoSnakeCaseUsage
+const (
+	NULL string = "NULL"
+)
+
+//goland:noinspection GoSnakeCaseUsage
+var (
+	NULL_AS_BYTES_SLICE = []byte(NULL)
+)
