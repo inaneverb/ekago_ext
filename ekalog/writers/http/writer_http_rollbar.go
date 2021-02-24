bf1d0abc849f9a585134c5e339d2377499795e94
Ilya Stroy <iyuryevich@pm.me>
2021-02-24 22:20:40 +0300

    CI_WriterHttp: Added Rollbar support, Bump ekago -> 2.9.9

---
+++ ekalog/writers/http/writer_http_rollbar.go
@@ -0,0 +1,40 @@
+// Copyright © 2021. All rights reserved.
+// Author: Ilya Stroy.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
+package ekalog_writer_http
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/qioalice/ekago/v2/ekastr"
+
+	"github.com/valyala/fasthttp"
+)
+
+//noinspection GoSnakeCaseUsage
+const (
+	ROLLBAR_ADDR = "https://api.rollbar.com/api/1/items/"
+)
+
+func (dw *CI_WriterHttp) UseProviderRollbar(token string) *CI_WriterHttp {
+
+	var (
+		JsonBodyStart = ekastr.S2B(`"access_token":"` + token + `",data:`)
+		JsonBodyEnd   = ekastr.S2B(`}`)
+	)
+
+	cb1 := func(req *fasthttp.Request) {
+		req.SetRequestURI(ROLLBAR_ADDR)
+		req.Header.SetContentType("application/json")
+	}
+
+	cb2 := func(oldBody io.Reader) io.Reader {
+		r := bytes.NewReader
+		return io.MultiReader(r(JsonBodyStart), oldBody, r(JsonBodyEnd))
+	}
+
+	return dw.AddBeforeAfterBetweenS("[],").UseProviderManual(cb1, cb2)
+}
